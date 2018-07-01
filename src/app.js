401256f26aab705a49c30ba914ec0d334a5905e3
adil1214 <adil1350@gmail.com>
2018-07-01 21:25:53 +0100

    passing children to components

--- src/app.js
+++ src/app.js
@@ -2,6 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecesionApp from './components/IndecesionApp'
 
+const Layout = (props) => {
+    return (
+        <div>
+            <p>header</p>
+                {props.children}        // 
+            <p>footer</p>
+        </div>
+    );
+};
+
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>this is my page</p>
+    </div>
+)
 
 let appRoot = document.getElementById('app');
-ReactDOM.render(<IndecesionApp />, appRoot);
+
+ReactDOM.render((
+        <Layout>
+            <p>this is inline</p>
+        </Layout>
+    ), appRoot);
