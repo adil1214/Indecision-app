2aa69890312e7c84136614813905dfe0c8c39f08
adil1214 <adil1350@gmail.com>
2018-07-03 21:03:47 +0100

    explicitly returning the jsx in the stateless functional components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.text}
-            <button
-                onClick={(e) => {
-                    props.handleDeleteOption(props.text)
-                }}
-            >
-                remove
-            </button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        {props.text}
+        <button
+            onClick={(e) => {
+                props.handleDeleteOption(props.text)
+            }}
+        >
+            remove
+        </button>
+    </div>
+);
 
 export default Option;
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-    return (
-        <div>
-            <button onClick={props.handleDeleteOptions}>Clear List</button>
-            {props.options.length === 0 && <p>Please add an options to get started.</p> }
-            {props.options.map((value, key) => (
-                <Option
-                    key={key}
-                    text={value}
-                    handleDeleteOption={props.handleDeleteOption}
-                />
-            ))}
-        </div>
-    )
-}
+const Options = (props) => (
+    <div>
+        <button onClick={props.handleDeleteOptions}>Clear List</button>
+        {props.options.length === 0 && <p>Please add an options to get started.</p> }
+        {props.options.map((value, key) => (
+            <Option
+                key={key}
+                text={value}
+                handleDeleteOption={props.handleDeleteOption}
+            />
+        ))}
+    </div>
+);
 
 export default Options;
