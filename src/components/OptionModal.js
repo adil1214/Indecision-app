9208595a0875cad13948848e4b57f62d998307b9
adil1214 <adil1350@gmail.com>
2018-07-07 19:36:21 +0100

    added react-modal transition.

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,9 +5,10 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
     <Modal
         isOpen={!!props.optionSelected}
-        contentLabel="Selected Option :D"
+        contentLabel="Selected Option"
         shouldCloseOnOverlayClick={true}
         onRequestClose={props.handleClearSelectedOption}
+        closeTimeoutMS={200}
     >
         <h3>Selected option</h3>
         {props.optionSelected && <p>{props.optionSelected}</p> }
